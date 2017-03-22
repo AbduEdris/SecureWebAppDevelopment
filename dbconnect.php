61ecb8b578074b45669e79823e40733b0b71b68c
Abdu Endris <abdu.mohammed@ogr.sakarya.edu.tr>
2017-03-22 11:50:19 +0200

    Add files via upload

---
+++ dbconnect.php
@@ -0,0 +1,13 @@
+<?php
+ 
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "studentinfo";
+
+$conn = mysqli_connect($host,$username,$password,$dbname);
+/*if($conn)
+	echo "Database Connected";
+else
+	echo "Connection Failed";*/
